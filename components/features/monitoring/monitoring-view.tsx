3214930b71d2ee972dfc806e0a685096cfe5dd88
KangAthiwat-Dev <kang.athiwat.dev@gmail.com>
2025-12-18 14:24:00 +0700

    Updated monitoring view

--- components/features/monitoring/monitoring-view.tsx
+++ components/features/monitoring/monitoring-view.tsx
@@ -178,7 +178,7 @@ export default function MonitoringView({ users, initialFocusId }: MonitoringView
 
                             <GoogleMap
                                 mapContainerStyle={containerStyle}
-                                zoom={8}
+                                zoom={0.5}
                                 mapTypeId={mapType}
                                 onLoad={(map) => { mapRef.current = map; }} 
                                 options={{ 
